bd365ea6e19d77a1875a101e0761d2b9302a2fa8
Salome Castro Vieira <salome.casvi@gmail.com>
2017-11-17 14:21:31 -0500

    ad p tag in boby

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
-<html>
+<html lang="es">
 <head>
 <meta charset="UTF-8">
 <title>aprender en vim</title>
 </head>
 <body>
 <h1>NO LO PUEDO CREER QUE VAINA MAS MALA</H1>
-
+<p>
+	QUE BOBADA ESTO ENSERIO 
+	NO ME IMPORTA SI DAS CLASE 
+	OK!!!!!!!!
+</p>
+<strong>brando esta perdido</strong>
 </body>
 
 </html>
